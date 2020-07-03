56b48324e2816bcc3c18a56c9ff547a431a371b6
kelvinOfoli <ofolikelvin@gmail.com>
2020-07-03 15:53:05 +0000

    finished

--- routes/convert.js
+++ routes/convert.js
@@ -6,7 +6,7 @@ const YoutubeMp3Downloader = require("youtube-mp3-downloader");
 
 var YD = new YoutubeMp3Downloader({
     "ffmpegPath": "/ffmpeg/ffmpeg",        // Where is the FFmpeg binary located?
-    "outputPath": `${path.join(__dirname,'..', 'mp3')}`,    // Where should the downloaded and encoded files be stored?
+    "outputPath": `${path.join(__dirname, '..', 'mp3')}`,    // Where should the downloaded and encoded files be stored?
     "youtubeVideoQuality": "highest",       // What video quality should be used?
     "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
     "progressTimeout": 2000                 // How long should be the interval of the progress reports
@@ -27,24 +27,24 @@ function youtube_parser(url) {
 
 router.use('/convert', (req, res, next) => {
     const url = req.body.url;
-        
+
     YD.download(youtube_parser(url));
 
     YD.on("finished", function (err, data) {
-        console.log(JSON.stringify(data));
+        const filePath = data.file;
+        res.download(filePath);
     });
 
     YD.on("error", function (error) {
         console.log(error);
     });
 
-    YD.on("progress", function(progress) {
-        
-        // const percentage = JSON.parse(progress).percentage.percentage;
-        console.log(JSON.stringify(progress));
-        
+    YD.on("progress", function (progress) {
+
+        console.log(progress.progress.percentage);
+
     });
-    res.redirect('/');
+    
 })
 
 module.exports = router
