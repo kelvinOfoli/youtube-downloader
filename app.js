efe5e4c6f18f5e18d199414d49ad40a544e1e7e5
kelvinOfoli <ofolikelvin@gmail.com>
2020-07-06 14:45:01 +0000

    changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const notFoundRoute = require('./routes/404');
 const expresshbs = require("express-handlebars");
 const dotenv = require('dotenv')
 
-dotenv.config({ path : './config/config.env'})
+// dotenv.config({ path : './config/config.env'})
 
 const app = express();
 
